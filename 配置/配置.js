41f2a1ec38faa82271c896739e8a7cc5d478daee
roranrui <93120773+roranrui@users.noreply.github.com>
2022-12-24 23:56:09 +0800

    second

---
+++ 配置/配置.js
@@ -0,0 +1,39 @@
+var config = {
+    // 网页标题
+    title: "圣诞快乐！",
+    // 开始的ToXXX自定义
+    toSb: {
+        // 自定义Toxxx内容
+        content: "To君歌, 桃桃",
+        // 字体颜色 推荐格式1.单词 格式2. #333333
+        fontColor: "#901d22",
+        // 字体大小
+        fontSize: 40,
+        // 是否加粗，默认0不加粗，1加粗
+        fontWeight: 1,
+        // 向左右移动 正右移 负左移
+        leftOrRight: 0,
+        // 向上下移动 正下移 负上移
+        topOrBottom: 50
+    },
+    // 礼物大小
+    gift: {
+        width: 350,
+        height: 350
+    },
+    // 确定音乐后加载文案
+    loading: "加载中...",
+    // 圣诞树出来后要说的话
+    talks: {
+        // 注意，注意，注意这里标点符号使用英文输入法
+        content: ["圣诞快乐!", "Merry Christmas!", "每天都要开心", "♥"],
+        // 字体大小
+        fontSize: 40,
+        // 文字颜色 推荐格式1.单词 格式2. #333333
+        fontColor: "pink",
+        // 向左右移动 正右移 负左移
+        leftOrRight: 0,
+        // 向上下移动 正下移 负上移
+        topOrBottom: 400
+    },
+}
